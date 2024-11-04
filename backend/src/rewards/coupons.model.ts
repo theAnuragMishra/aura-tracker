6e484cb5388e2d8556e28ea8c89fbd4945112043
Anurag <coleridgeprotube@gmail.com>
2024-11-05 00:32:55 +0530

    add reward apis for coupons

--- backend/src/rewards/coupons.model.ts
+++ backend/src/rewards/coupons.model.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const couponSchema = new mongoose.Schema({
   code: {
@@ -24,6 +24,4 @@ const couponSchema = new mongoose.Schema({
   },
 });
 
-const Coupon = mongoose.model('Coupon', couponSchema);
-
-module.exports = Coupon;
+export const Coupon = mongoose.model("Coupon", couponSchema);
