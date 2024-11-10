1cc1fa3e09dba40a127c153a71b8e86af6059726
Anurag <coleridgeprotube@gmail.com>
2024-11-10 05:58:08 +0530

    feat: image upload for lost items

--- frontend/app/(all)/lostfound/add/page.tsx
+++ frontend/app/(all)/lostfound/add/page.tsx
@@ -1,51 +1,77 @@
 "use client";
 import { addItem } from "@/lib/lnfActions";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function AddItemPage() {
   const [itemName, setItemName] = useState("");
   const [description, setDescription] = useState("");
-  const [imageURL, setImageURL] = useState("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
+  const router = useRouter();
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // Get the selected file
+    if (file) {
+      setSelectedFile(file); // Update state with the selected file
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!itemName) return;
-    const newItem = {
-      itemName,
-      description,
-      imageURL,
-    };
+    try {
+      setUploading(true);
+      if (!itemName || !selectedFile) return;
 
-    await addItem(newItem);
+      if (selectedFile.size > 5 * 1024 * 1024) {
+        throw new Error("Avatar size must be 5mb or less");
+      }
+
+      const newItem = {
+        itemName,
+        description,
+      };
+
+      await addItem(newItem, selectedFile);
+      router.push("/lostfound");
+    } catch (error) {
+      alert(error);
+    } finally {
+      setUploading(false);
+    }
   };
 
   return (
     <div className="bg-gray-900 text-white p-8">
       <h1 className="text-2xl mb-4">Report Lost Item</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
+        <label htmlFor="item_name"></label>
         <input
           type="text"
+          id="item_name"
           value={itemName}
           onChange={(e) => setItemName(e.target.value)}
           placeholder="Item Name"
           className="w-full p-2 rounded bg-slate-200 border border-gray-900 text-black"
           required
         />
+        <label htmlFor="description"></label>
         <input
           type="text"
           value={description}
+          id="description"
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Describe your item"
           className="w-full p-2 rounded bg-slate-200 border border-gray-900 text-black"
           required
         />
+        <label htmlFor="image">Add a descriptive image: </label>
         <input
-          type="url"
-          value={imageURL}
-          onChange={(e) => setImageURL(e.target.value)}
-          placeholder="Image URL"
-          className="w-full p-2 rounded bg-slate-200 border border-gray-900 text-black"
-          required
+          id="image"
+          type="file"
+          accept="image/*"
+          disabled={uploading}
+          onChange={handleFileChange}
         />
         <button
           type="submit"
--- frontend/lib/lnfActions.ts
+++ frontend/lib/lnfActions.ts
@@ -6,28 +6,61 @@ import { getBaseURL, getUserDetails } from "./utils";
 import { getCurrentSession } from "./auth";
 
 import { revalidatePath } from "next/cache";
+import { createSupabaseClientJWT } from "./supabase-client";
 
-export async function addItem(item: {
-  itemName: string;
-  description: string;
-  imageURL: string;
-}) {
+export async function addItem(
+  item: { itemName: string; description: string },
+  file: File
+) {
+  if (file.size > 5 * 1024 * 1024) {
+    throw new Error("file size exceeded");
+  }
+
   const { user } = await getCurrentSession();
   const userData = await getUserDetails(user!);
+
+  const fileExt = file.name.split(".").pop();
+  const filePath = `${user!.id}-${Math.random()}.${fileExt}`;
+
   const token = jwt.sign(user!, process.env.JWT_SECRET!, {
     expiresIn: "10m",
   });
 
-  await axios.post(
-    `${getBaseURL()}/api/lnf/items/add`,
-    JSON.stringify({ ...item, owner: userData.username }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  const supabase = await createSupabaseClientJWT();
+  const { error: uploadError } = await supabase.storage
+    .from("lnf_items")
+    .upload(filePath, file);
+  if (uploadError) {
+    console.error(uploadError);
+    throw new Error("couldn't upload image");
+  }
+
+  const { data, error } = await supabase.storage
+    .from("lnf_items")
+    .createSignedUrl(filePath, 60 * 60 * 24 * 365);
+  if (error) {
+    console.error(error);
+    throw new Error("couldn't upload image");
+  }
+
+  const imageURL = data?.signedUrl;
+
+  try {
+    await axios.post(
+      `${getBaseURL()}/api/lnf/items/add`,
+      JSON.stringify({ ...item, owner: userData.username, imageURL }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    revalidatePath("/lostfound");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function toggleFindClaim(id: number, username: string) {
